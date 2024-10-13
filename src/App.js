0e26587d0322ea24acf5176e3eae3247dbcdff56
Abhishek Singh <abhi@Abhisheks-MacBook-Pro.local>
2024-10-13 16:55:31 +0530

    typeahead search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useCallback, useEffect, useState } from "react";
+
+const debounce = (func, delay) => {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState("");
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const debouncedSetQuery = useCallback(
+    debounce((value) => setDebouncedQuery(value), 500),
+    []
+  );
+
+  const fetchData = async () => {
+    console.log("i am called");
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products/search?q=${debouncedQuery}`
+      );
+      const result = await response.json();
+      setData(result.products);
+      setLoading(false);
+    } catch (e) {
+      console.log("error", e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+    debouncedSetQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [debouncedQuery]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Auto Complete / Auto Suggest / TypeHead</h1>
+      <input
+        type="text"
+        value={query}
+        placeholder="Searc.."
+        onChange={(e) => handleInputChange(e)}
+      />
+      {loading && <p>Loading...</p>}
+      <ul>
+        {data.map((item) => {
+          return (
+            <li
+              key={item.id}
+              onClick={() => {
+                setQuery(item.title);
+                debouncedSetQuery(item.title);
+              }}
+            >
+              {item.title}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
